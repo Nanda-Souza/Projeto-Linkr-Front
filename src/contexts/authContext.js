a24c87fff4aa96c90b8ba166e935a2d276b638b0
Leonan <leonanm04@gmail.com>
2023-03-07 20:24:26 -0300

    feat: sign-in functional with API

---
+++ src/contexts/authContext.js
@@ -0,0 +1,61 @@
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const recoveredUser = localStorage.getItem("user");
+    const recoveredToken = localStorage.getItem("tokenUser");
+
+    if (recoveredUser && recoveredToken) {
+      setUser(JSON.parse(recoveredUser));
+      setToken(JSON.parse(recoveredToken));
+    }
+
+    setLoading(false);
+  }, []);
+
+  const login = (data) => {
+    const loggedUser = data;
+    const token = data.token;
+    delete loggedUser.token;
+
+    localStorage.setItem("user", JSON.stringify(loggedUser));
+    localStorage.setItem("tokenUser", JSON.stringify(token));
+
+    setUser(loggedUser);
+    setToken(token);
+    navigate("/");
+  };
+
+  const logout = () => {
+    console.log("Você saiu!");
+    localStorage.removeItem("user");
+    localStorage.removeItem("tokenUser");
+    setUser(null);
+    setToken(null);
+    navigate("/");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        authenticated: !!user,
+        user,
+        token,
+        login,
+        loading,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
