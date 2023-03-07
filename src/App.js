a85fcfb4ddd6c5507109f1c9edbec87212b1d28a
Leonan <leonanm04@gmail.com>
2023-03-07 10:10:15 -0300

    refactor: remove unused files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignInPage from "./pages/signIn/signInPage";
+import SignUpPage from "./pages/signUp/signUpPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign-in" element={<SignInPage />}></Route>
+          <Route path="/sign-up" element={<SignUpPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
